dbcb9ffcedf24766111d37f52e47af02eaa825bb
JCxYIS <jsn811@gmail.com>
2018-04-29 19:55:29 +0800

    mod coding enviroment

--- Kart_gen3_ForCompetition.ino
+++ Kart_gen3_ForCompetition.ino
@@ -1,25 +1,34 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial BT(10, 11); // RX | TX
-char val;  // 儲存接收資料的變數
+char mode;
 
-void setup() {
+void setup() 
+{
   Serial.begin(9600);   // 與電腦序列埠連線
   Serial.println("BT is ready! type AT to set func");
 
-  // 設定藍牙模組的連線速率
-  BT.begin(9600);
+  BT.begin(9600);// 設定藍牙模組的連線速率
 }
 
-void loop() {
+void loop() 
+{
+  BlueToothHandler();
+}
+
+//這裡處理藍芽事宜
+void BlueToothHandler()
+{
   // 若收到「序列埠監控視窗」的資料，則送到藍牙模組
-  if (Serial.available()) {
-    val = Serial.read();
+  if (Serial.available()) 
+  {
+    char val = Serial.read();
     BT.print(val);
   }
 
   // 若收到藍牙模組的資料，則送到「序列埠監控視窗」
-  if (BT.available()) {
+  if (BT.available())
+  {
     val = BT.read();
     Serial.print(val);
   }
