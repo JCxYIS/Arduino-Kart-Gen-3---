3a951af7eb81fc71481dfb032cb4cbd764766214
JCxYIS <jsn811@gmail.com>
2018-05-21 20:40:07 +0800

    updated sketch

--- Kart_gen3_ForCompetition.ino
+++ Kart_gen3_ForCompetition.ino
@@ -3,12 +3,12 @@
 #include <Servo.h>
 
 SoftwareSerial BT(2, 13); // RX | TX , (2,13) are not used by motor shield
-AF_DCMotor motorL(1), motorR(4), motorL2(2), motorR2(3); //接腳座及頻率
+AF_DCMotor motorL(1), motorR(4), motorM(2); //接腳座及頻率
 Servo servo;
 
 char command[20];
 int commandBuffer = 0;//income command should be written on which pos of command[]?
-int speedL = 0, speedR = 0;
+int speedL = 0, speedR = 0, speedM = 0;
 int servoAngle = 0, servoPreAngle = 0;
 
 void setup() 
@@ -24,12 +24,14 @@ void setup()
   motorL.run(RELEASE);
   motorR.setSpeed(0);    //可調轉速約150~到255
   motorR.run(RELEASE);
+  motorM.setSpeed(0);    //可調轉速約150~到255
+  motorM.run(RELEASE);
 }
 
 void loop() 
 {
   BlueToothHandler();
-  MotorMovement(speedL, speedR);
+  MotorMovement(speedL, speedR, speedM);
   ServoBehavior();
 }
 
@@ -73,11 +75,13 @@ void CommandHandler()
     case ' ':
     break;
 
-    case 'M'://mod Motor speed, 001~205 //EX: M-048+163
+    case 'M'://mod Motor speed, 001~255 //EX: M-048+163
       speedL = ((int)command[2]-'0')*100 + ((int)command[3]-'0')*10 + ((int)command[4]-'0');
       speedR = ((int)command[6]-'0')*100 + ((int)command[7]-'0')*10 + ((int)command[8]-'0');
+      speedM = ((int)command[10]-'0')*100 + ((int)command[11]-'0')*10 + ((int)command[12]-'0');
       if(command[1] == '-'){speedL *= -1;}
       if(command[5] == '-'){speedR *= -1;}
+      if(command[9] == '-'){speedR *= -1;}
     break;
 
     case 'S': //set servo angle  //EX: S+152
@@ -95,50 +99,55 @@ void CommandHandler()
   }
 }
 //負責馬達
-void MotorMovement(int l, int r)
+void MotorMovement(int l, int r, int m)
 {
   if(l < 0)
   {
     l *= -1;
     motorL.setSpeed(l);
-    motorL2.setSpeed(l);
     motorL.run(BACKWARD);
-    motorL2.run(BACKWARD);
   }
   else if(l > 0)
   {
     motorL.setSpeed(l);
-    motorL2.setSpeed(l);
     motorL.run(FORWARD);
-    motorL2.run(FORWARD);
   }
   else
   {
     motorL.setSpeed(0);
-    motorL2.setSpeed(0);
     motorL.run(RELEASE);
-    motorL2.run(RELEASE);
   }
   if(r < 0)
   {
     r *= -1;
     motorR.setSpeed(r);
     motorR.run(BACKWARD);
-    motorR2.run(BACKWARD);
   }
   else if(r > 0)
   {
     motorR.setSpeed(r);
-    motorR2.setSpeed(r);
     motorR.run(FORWARD);
-    motorR2.run(FORWARD);
   }
   else
   {
     motorR.setSpeed(0);
-    motorR2.setSpeed(r);
     motorR.run(RELEASE);
-    motorR2.run(RELEASE);
+  }
+  if(m < 0)
+  {
+    m *= -1;
+    motorM.setSpeed(r);
+    motorM.run(BACKWARD);
+  }
+  else if(m > 0)
+  {
+    motorM.setSpeed(r);
+    motorM.run(FORWARD);
+  }
+  else
+  {
+    motorM.setSpeed(0);
+    motorM.run(RELEASE);
   }
   //Serial.print("MOTOR=");Serial.print(l);Serial.print(",");Serial.print(r);Serial.print("\n");
 }
